a70f4ddae97a0f53578a43582833882c6cf4bec9
tsukimi <128211112+hikawi@users.noreply.github.com>
2024-10-23 21:23:00 +0700

    fix i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,19 +1,4 @@
-import { $locale } from "@/stores/i18n";
-import { browser, createI18n, localeFrom } from "@nanostores/i18n";
-
-const localeStore = localeFrom(
-  $locale,
-  browser({
-    available: ["en", "ja", "vi"],
-    fallback: "en",
-  }),
-);
-
-const i18n = createI18n(localeStore, {
-  get(code) {
-    return import(/* @vite-ignore */ `./${code}.json`);
-  },
-});
+import { i18n } from "@s/i18n";
 
 const $clockSwitcher = i18n("clockSwitcher", {
   radioGroup: "Pick a clock type",
--- src/stores/i18n.ts
+++ src/stores/i18n.ts
@@ -1,5 +1,20 @@
+import { browser, createI18n, localeFrom } from "@nanostores/i18n";
 import { persistentAtom } from "@nanostores/persistent";
 
 const $locale = persistentAtom<string>("locale", "en");
 
-export { $locale };
+const localeStore = localeFrom(
+  $locale,
+  browser({
+    available: ["en", "ja", "vi"],
+    fallback: "en",
+  }),
+);
+
+const i18n = createI18n(localeStore, {
+  get(code) {
+    return import(`../i18n/${code}.json`);
+  },
+});
+
+export { $locale, i18n };
